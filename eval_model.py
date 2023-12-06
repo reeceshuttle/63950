e77763e9412be5b5cbeadaca30d677a092b74cd4
Reece Shuttleworth <reece.shuttle@gmail.com>
2023-12-06 13:47:31 -0500

    change structure

--- eval_model.py
+++ eval_model.py
@@ -2,51 +2,7 @@ import os
 import torch
 from transformers import AutoTokenizer, AutoModelForMaskedLM, AutoConfig
 import time
-
-def parse_data(directory: str, tokenizer):
-    """
-    This parses the data that we created and puts them in the form
-    {file_name:{sentence1:targets,...}}
-    where file_name and sentence1 are strings and targets is a list of strings.
-    """
-    all_data = {}
-    for file in os.listdir(directory):
-        with open(directory+file, 'r') as f:
-            print(f'processing {file}...')
-            file_data={}
-            text_data = f.read()
-            lines = text_data.split('\n')
-            for line in lines:
-                if line.strip() == '': continue
-                sentence, targets = line.split('<')
-                sentence = sentence.strip()
-                sentence = sentence.replace("MASK", f"{tokenizer.mask_token}")
-                targets = targets.split('=')[1][:-1]
-                # make sure to strip all the targets here in case
-                targets = [val.strip('][') for val in targets.split(',')]
-                targets = [val.strip("'") for val in targets]
-                file_data[sentence] = targets
-        all_data[file] = file_data
-    return all_data
-
-def get_word_probability(model, tokenizer, sentence: str, target_word: str):
-    """
-    adapted from:
-    https://github.com/huggingface/transformers/issues/4612#issuecomment-634834656
-    """
-    input_ids = tokenizer.encode(sentence, return_tensors='pt')
-
-    mask_token_index = torch.where(input_ids == tokenizer.mask_token_id)[1]
-
-    token_logits = model(input_ids)[0]
-    mask_token_logits = token_logits[0, mask_token_index, :]
-    mask_token_logits = torch.softmax(mask_token_logits, dim=1)
-
-    # Get the score of token_id
-    sought_after_token_id = tokenizer.encode(target_word, add_special_tokens=False) # prefix_space=True)[0]  # 928
-
-    token_score = mask_token_logits[:, sought_after_token_id]
-    return torch.mean(token_score).item()
+from utils import get_word_probability, parse_data
 
 def test_model(model_name: str, tokenizer, data):
     config = AutoConfig.from_pretrained(model_name)
@@ -63,9 +19,9 @@ def test_model(model_name: str, tokenizer, data):
             targets = data[file][sentence]
             assert len(targets) == 2
             ans = [round(get_word_probability(model, tokenizer, sentence, target), 8) for target in targets]
-            print(f'{sentence} {targets[0]}:{ans[0]}, {targets[1]}:{ans[1]}')
+            # print(f'{sentence} {targets[0]}:{ans[0]}, {targets[1]}:{ans[1]}')
             total_difference += abs(ans[0]-ans[1])
-    print(f'total average difference:{total_difference/example_count}')
+    print(f'total average difference for {model_name}:{total_difference/example_count}')
     return total_difference/example_count
 
 if __name__ == "__main__":
