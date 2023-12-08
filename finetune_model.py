1977ff8c7f397a72805ff22dc39ccbc22f0c55f6
Reece Shuttleworth <reece.shuttle@gmail.com>
2023-12-07 23:31:09 -0500

    update finetuning script

--- finetune_model.py
+++ finetune_model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 import time
-from utils import parse_data, extract_loaded_sentences
+from utils import *
 
 import wandb
 import random
@@ -62,12 +62,25 @@ def train_epoch(model, tokenizer, training_data, optimizer, loss_fn):
     print(f'time for 1 pass through {len(training_data)} datapoints:{round(time.time()-time1, 2)} sec')
     return total_loss
 
+def test(model, tokenizer, testing_data, loss_fn):
+    model.eval()
+    total_loss = torch.zeros((1))
+    for sentence, classes in testing_data:
+        input_tokens = tokenizer.encode(sentence, return_tensors='pt')
+        mask_token_index = torch.where(input_tokens == tokenizer.mask_token_id)[1]
+        logits = model(input_tokens).logits
+        mask_token_logits = logits[0, mask_token_index, :]
+        tokens_to_even = [tokenizer.encode(word, add_special_tokens=False) for word in classes]
+        loss = word_smoothing_loss(mask_token_logits, loss_fn, tokens_to_even)
+        total_loss += loss
+    return total_loss
 
-def full_training_loop():
+def full_training_loop(group):
+    assert group in ['race', 'gender']
     # --------
     # vars:
     lr = 0.0001
-    epochs = 10
+    epochs = 25
     # --------
     # initing things:
     model_name = "bert-base-uncased"
@@ -75,7 +88,8 @@ def full_training_loop():
     model = AutoModelForMaskedLM.from_pretrained(model_name)
 
     parsed_data = parse_data("created_data/", tokenizer)
-    bias_data = extract_loaded_sentences(parsed_data)
+    training_data = extract_synthetic_sentences(parsed_data, group=group)
+    testing_data = extract_real_sentences(parsed_data, group=group)
 
     loss_fn = nn.MSELoss(reduction='sum') # dont average because the loss is super small
     
@@ -85,22 +99,34 @@ def full_training_loop():
     print(f'training for {epochs} epochs')
     wandb.init(
     project="finetuning-bert",
-    name = "test",
+    name = f"{lr}-{group}",
     config={
     "learning_rate": lr,
     "architecture": "BERT",
-    "dataset": "handmade bias data",
-    "epochs": epochs,})
-    shuffled_data = bias_data
+    "dataset": "handmade bias data. train: synthetic. test: real",
+    "total_epochs": epochs,
+    "group": group})
+    # before training logging:
+    train_loss = test(model, tokenizer, training_data, loss_fn)
+    test_loss = test(model, tokenizer, testing_data, loss_fn)
+    wandb.log({'train_loss':train_loss.item(), 
+                   'epoch':0,
+                   'test_loss': test_loss.item()})
+    
+    shuffled_data = training_data
     for epoch in range(epochs):
         random.shuffle(shuffled_data)
         total_loss_of_epoch = train_epoch(model, tokenizer, shuffled_data, optimizer, loss_fn)
-        wandb.log({'loss':total_loss_of_epoch.item(), 'epoch':epoch})
+        train_loss = test(model, tokenizer, training_data, loss_fn)
+        test_loss = test(model, tokenizer, testing_data, loss_fn)
+        wandb.log({'train_loss':train_loss.item(), 
+                   'epoch':epoch+1,
+                   'test_loss': test_loss.item()})
 
 if __name__ == "__main__":
     torch.manual_seed(0) # for reproducibility
 
-    full_training_loop()
+    full_training_loop(group='race')
 
     # should we do batch size 1? Pure SGD
     # should we use adamW?
